e5f61f2fe2570ac1ddfbc3edabee83f9319e58ab
Farbod <cppoxking@gmail.com>
2022-08-15 20:40:20 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -3,10 +3,33 @@ import requests,os,json
 #session = os.environ.get("TOKEN")
 lat = os.environ.get("LAT")
 long = os.environ.get("LONG")
-
+    
+def send(bot_message):
+       bot_token = os.environ.get("TOKEN")
+       bot_chatID = os.environ.get("CHATID")
+       send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    
+       response = requests.get(send_text)
+    
+       return response.json()
+    
 x = requests.get("https://foodparty.zoodfood.com/676858d198d35e7713a47e66ba0755c8/mobile-offers/"+str(lat)+"/"+str(long)+"?superType=1")
 y = json.loads(x.text)
 #print(y["data"]["products"][0]) 
+goh = ""
 for ghaza in y["data"]["products"]:
   if ghaza["discountRatio"] >= 10:
     print(ghaza["vendorCode"])
+    f = open("goh.txt", "r")
+    if ghaza["productVariationId"] not in f.read():
+      send(ghaza["vendorCode"])
+    goh += ghaza["productVariationId"]+","
+    
+f = open("goh.txt", "w")
+f.write(goh)
+f.close()    
+    
+    
+    
+    
+ 
