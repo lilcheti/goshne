c77b32d67361afe30327a6988366daea15a9265b
Farbod <cppoxking@gmail.com>
2022-08-15 20:56:08 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -22,7 +22,7 @@ for ghaza in y["data"]["products"]:
     print(ghaza["vendorCode"])
     f = open("goh.txt", "r")
     if str(ghaza["productVariationId"]) not in str(f.read()):
-      send("["+ghaza["title"]+"](https://snappfood.ir/restaurant/menu/"+ghaza["vendorCode"]+") "+str(ghaza["price"]-ghaza["discount"])
+      send("["+ghaza["title"]+"](https://snappfood.ir/restaurant/menu/"+ghaza["vendorCode"]+") "+str(ghaza["price"]-ghaza["discount"]))
     goh += str(ghaza["productVariationId"])+","
     
 f = open("goh.txt", "w")
