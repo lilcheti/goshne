8ad98a325d7ecf6de36fef7fa8ec47920bc31761
Farbod <cppoxking@gmail.com>
2022-08-15 20:16:49 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -1,8 +1,10 @@
 
-import requests,os
+import requests,os,json
 #session = os.environ.get("TOKEN")
 lat = os.environ.get("LAT")
 long = os.environ.get("LONG")
 
 x = requests.get("https://foodparty.zoodfood.com/676858d198d35e7713a47e66ba0755c8/mobile-offers/"+str(lat)+"/"+str(long)+"?superType=1")
 print(x.text)
+y = json.loads(x.text)
+print(y["data"]["products"][0]) 
