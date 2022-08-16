afcf2607fff797bf934af174d28d72f51217682d
Farbod <cppoxking@gmail.com>
2022-08-16 09:29:23 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -1,8 +1,5 @@
 
 import requests,os,json
-
-lat = os.environ.get("LAT")
-long = os.environ.get("LONG")
     
 def send(bot_message):
        bot_token = os.environ.get("TOKEN")
@@ -12,20 +9,21 @@ def send(bot_message):
        response = requests.get(send_text)
     
        return response.json()
-
-send(str(os.environ.get("LOC_NAME")))
-    
-x = requests.get("https://foodparty.zoodfood.com/676858d198d35e7713a47e66ba0755c8/mobile-offers/"+str(lat)+"/"+str(long)+"?superType=1")
-y = json.loads(x.text)
-#print(y["data"]["products"][0]) 
 goh = ""
-for ghaza in y["data"]["products"]:
-  if ghaza["discountRatio"] >= 40:
-    print(ghaza["vendorCode"])
-    f = open("goh.txt", "r")
-    if str(ghaza["productVariationId"]) not in str(f.read()):
-      send("["+ghaza["title"]+"](https://m.snappfood.ir/selectSideDish/"+ghaza["productVariationId"]+") "+str(ghaza["price"]-ghaza["discount"]+ghaza["vendorContainerFee"]+int(ghaza["deliveryFee"])))
-    goh += str(ghaza["productVariationId"])+","
+for i in range(3):
+    send(str(os.environ.get("LOC"+str(i+1))))
+    lat = os.environ.get("LAT"+str(i+1))
+    long = os.environ.get("LONG"+str(i+1))
+    x = requests.get("https://foodparty.zoodfood.com/676858d198d35e7713a47e66ba0755c8/mobile-offers/"+str(lat)+"/"+str(long)+"?superType=1")
+    y = json.loads(x.text)
+    #print(y["data"]["products"][0]) 
+    for ghaza in y["data"]["products"]:
+      if ghaza["discountRatio"] >= 40:
+        print(ghaza["vendorCode"])
+        f = open("goh.txt", "r")
+        if str(ghaza["productVariationId"]) not in str(f.read()):
+          send("["+ghaza["title"]+"](https://m.snappfood.ir/selectSideDish/"+str(ghaza["productVariationId"])+") "+str(ghaza["price"]-ghaza["discount"]+ghaza["vendorContainerFee"]+int(ghaza["deliveryFee"])))
+        goh += str(ghaza["productVariationId"])+","
     
 f = open("goh.txt", "w")
 f.write(goh)
