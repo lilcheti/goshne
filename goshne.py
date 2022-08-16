badb0a78dbc3330bbe3a1ebcad09097213e4d9fb
Farbod <cppoxking@gmail.com>
2022-08-16 09:21:39 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -12,6 +12,8 @@ def send(bot_message):
        response = requests.get(send_text)
     
        return response.json()
+
+send(str(os.environ.get("LOC_NAME")))
     
 x = requests.get("https://foodparty.zoodfood.com/676858d198d35e7713a47e66ba0755c8/mobile-offers/"+str(lat)+"/"+str(long)+"?superType=1")
 y = json.loads(x.text)
