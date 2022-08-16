e987e5e3b7bbe21370e66c5b9f76b940d76d48e7
Farbod <cppoxking@gmail.com>
2022-08-16 09:40:01 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -4,7 +4,7 @@ import requests,os,json
 def send(bot_message):
        bot_token = os.environ.get("TOKEN")
        bot_chatID = os.environ.get("CHATID")
-       send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+       send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&disable_web_page_preview=true&text=' + bot_message
     
        response = requests.get(send_text)
     
