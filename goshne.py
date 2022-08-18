ecbde640dd90927160e906e41960491e737ff09c
Farbod <cppoxking@gmail.com>
2022-08-18 16:12:16 +0430

    Update goshne.py

--- goshne.py
+++ goshne.py
@@ -10,6 +10,7 @@ def send(bot_message):
     
        return response.json()
 goh = ""
+g = open(str(os.environ.get("CHATID"))+".txt", "w+")
 for i in range(int(sys.argv[1])):
     lat = os.environ.get("LAT"+str(i+1))
     long = os.environ.get("LONG"+str(i+1))
@@ -23,10 +24,8 @@ for i in range(int(sys.argv[1])):
         if str(ghaza["productVariationId"]) not in str(f.read()):
           send("["+ghaza["title"]+"](https://m.snappfood.ir/selectSideDish/"+str(ghaza["code"])+") %0A"+str(ghaza["price"]-ghaza["discount"]+ghaza["vendorContainerFee"]+int(ghaza["deliveryFee"]))+"%0A"+str(os.environ.get("LOC"+str(i+1)))+"%0A"+"["+ghaza["vendorTitle"]+"](https://m.snappfood.ir/restaurant/"+ghaza["vendorCode"]+")")
         goh += str(ghaza["productVariationId"])+","
-    
-f = open(str(os.environ.get("CHATID"))+".txt", "w+")
-f.write(goh)
-f.close()    
+g.write(goh)
+g.close()    
     
     
     
